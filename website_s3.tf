af2aec8af98169d1fe278894a06201f9cd8bfb42
saulthompson <saulthompson@hotmail.co.uk>
2025-03-13 18:42:39 -0500

    added a tf module with provisioning and policy for an s3 bucket which will host our website

---
+++ website_s3.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "website" {
+  bucket = "my-static-site-${local.account_id}"
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.website.id
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.website.id}/*"
+    }
+  ]
+}
+POLICY
+}
