06ae1db77b3151e18419c9e6e5ec0e89f7df7811
saulthompson <saulthompson@hotmail.co.uk>
2025-03-15 14:01:36 -0500

    added public access block for the website-hosting s3 bucket

--- modules/website_s3/main.tf
+++ modules/website_s3/main.tf
@@ -11,6 +11,15 @@ resource "aws_s3_bucket" "website" {
   bucket = "my-static-site-${var.account_id}"
 }
 
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false  # Allow public bucket policies
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "public_read" {
   bucket = aws_s3_bucket.website.id
   policy = <<POLICY
@@ -26,4 +35,6 @@ resource "aws_s3_bucket_policy" "public_read" {
   ]
 }
 POLICY
+
+  depends_on = [aws_s3_bucket_public_access_block.website]
 }
