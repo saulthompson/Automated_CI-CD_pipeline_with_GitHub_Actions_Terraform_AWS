2debb1fa8974586969512099ec926cea2e11c016
saulthompson <saulthompson@hotmail.co.uk>
2025-03-16 18:06:11 -0500

    Update github_actions_integration to import existing GitHubActionsRole and Policy

--- modules/github_actions_integration/main.tf
+++ modules/github_actions_integration/main.tf
@@ -1,26 +1,42 @@
-resource "aws_iam_role" "github_actions" {
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws" version = "~> 5.0" }
+  }
+}
+
+variable "github_repo" { type = string }
+variable "account_id" { type = string }
+variable "bucket_arn" { type = string }
+
+# Reference existing role if it exists, otherwise create it
+data "aws_iam_role" "github_actions_existing" {
   name = "GitHubActionsRole"
+}
+
+resource "aws_iam_role" "github_actions" {
+  count = length(data.aws_iam_role.github_actions_existing) == 0 ? 1 : 0  # Create only if not found
+  name  = "GitHubActionsRole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::${var.account_id}:oidc-provider/token.actions.githubusercontent.com"
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/main"
-          }
-        }
+        Effect    = "Allow"
+        Principal = { Federated = "arn:aws:iam::${var.account_id}:oidc-provider/token.actions.githubusercontent.com" }
+        Action    = "sts:AssumeRoleWithWebIdentity"
+        Condition = { StringEquals = { "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/main" } }
       }
     ]
   })
 }
 
+# Reference existing policy if it exists, otherwise create it
+data "aws_iam_policy" "github_actions_policy_existing" {
+  name = "GitHubActionsPolicy"
+}
+
 resource "aws_iam_policy" "github_actions_policy" {
-  name        = "GitHubActionsPolicy"
+  count = length(data.aws_iam_policy.github_actions_policy_existing) == 0 ? 1 : 0  # Create only if not found
+  name  = "GitHubActionsPolicy"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -44,14 +60,14 @@ resource "aws_iam_policy" "github_actions_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "github_actions_attach" {
-  role       = aws_iam_role.github_actions.name
-  policy_arn = aws_iam_policy.github_actions_policy.arn
+  role       = length(data.aws_iam_role.github_actions_existing) == 0 ? aws_iam_role.github_actions[0].name : data.aws_iam_role.github_actions_existing.name
+  policy_arn = length(data.aws_iam_policy.github_actions_policy_existing) == 0 ? aws_iam_policy.github_actions_policy[0].arn : data.aws_iam_policy.github_actions_policy_existing.arn
 }
 
 resource "aws_iam_policy" "github_s3_upload" {
-  name        = "GitHubS3UploadPolicy"
+  name = "GitHubS3UploadPolicy"
   policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [{
       Effect   = "Allow"
       Action   = ["s3:PutObject", "s3:ListBucket"]
@@ -61,7 +77,7 @@ resource "aws_iam_policy" "github_s3_upload" {
 }
 
 resource "aws_iam_role_policy_attachment" "github_s3_attach" {
-  role       = aws_iam_role.github_actions.name
+  role       = length(data.aws_iam_role.github_actions_existing) == 0 ? aws_iam_role.github_actions[0].name : data.aws_iam_role.github_actions_existing.name
   policy_arn = aws_iam_policy.github_s3_upload.arn
 }
 
