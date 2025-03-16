f2426ff55ee570888d8707067d1353424c8bc9ca
saulthompson <saulthompson@hotmail.co.uk>
2025-03-16 17:52:59 -0500

    add universal iam permissions for GitHubActionsRole

--- modules/github_actions_integration/main.tf
+++ modules/github_actions_integration/main.tf
@@ -26,7 +26,7 @@ resource "aws_iam_policy" "github_actions_policy" {
     Statement = [
       {
         Effect   = "Allow"
-        Action   = "s3:*"
+        Action   = ["s3:*", "iam:*"]
         Resource = "*"
       },
       {
