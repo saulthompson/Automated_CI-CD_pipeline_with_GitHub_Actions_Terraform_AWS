58a1cb7ebbb6452f2660064f53d5c45961299a31
saulthompson <saulthompson@hotmail.co.uk>
2025-03-16 18:43:40 -0500

    removed github_actions_integrations from root terraform file

--- main.tf
+++ main.tf
@@ -22,13 +22,6 @@ resource "aws_s3_bucket" "tfstate" {
   bucket = "tfs-${var.account_id}"
 }
 
-module "github_actions_integration" {
-  source = "./modules/github_actions_integration"
-  github_repo = var.github_repo
-  bucket_arn = module.website_s3.bucket_arn
-  account_id = var.account_id
-}
-
 #module "cloudfront" {
 #  source = "./modules/cloudfront"
 #  bucket_name = module.website_s3.bucket_name
