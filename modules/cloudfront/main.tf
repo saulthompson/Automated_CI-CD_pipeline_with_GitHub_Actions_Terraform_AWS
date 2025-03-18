42be8c9296374a229a095cc0cde82eb3bc3e3cba
saulthompson <saulthompson@hotmail.co.uk>
2025-03-18 11:24:02 -0500

    setup cloudfront to use lambda edge function. remove lambda packaging from gha workflow and make cloudfront/main.tf handle lambda zipping

--- modules/cloudfront/main.tf
+++ modules/cloudfront/main.tf
@@ -1,20 +1,50 @@
 # Lambda@Edge for Basic Auth
+data "template_file" "lambda_auth" {
+  template = file("${path.module}/lambda/index.js.tpl")
+  vars = {
+    basic_user     = var.website_username
+    basic_password = var.website_password
+  }
+}
+
+resource "local_file" "lambda_auth_rendered" {
+  content  = data.template_file.lambda_auth.rendered
+  filename = "${path.module}/lambda/index.js"
+}
+
+resource "null_resource" "create_lambda_zip" {
+  triggers = {
+    source_code_hash = filebase64sha256("${path.module}/lambda/index.js")
+  }
+
+  provisioner "local-exec" {
+    command = "cd ${path.module}/lambda && zip -j ../lambda.zip index.js"
+  }
+
+ 
+
+  depends_on = [local_file.lambda_auth_rendered]
+}
+
 resource "aws_lambda_function" "basic_auth" {
   function_name    = "basic-auth-${var.account_id}"
   handler          = "index.handler"
   runtime          = "nodejs20.x"
   role             = aws_iam_role.lambda_exec.arn
-  filename         = "lambda.zip"
-  source_code_hash = filebase64sha256("lambda.zip")
+  filename         = "${path.module}/lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda.zip")
   publish          = true
+
+  depends_on = [null_resource.create_lambda_zip]
 }
 
-data "template_file" "lambda_auth" {
-  template = file("./lambda/index.js.tpl")
-  vars = {
-    basic_user     = var.website_username
-    basic_password = var.website_password
-  }
+resource "aws_lambda_permission" "allow_cloudfront" {
+  statement_id  = "AllowExecutionFromCloudFront"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.basic_auth.function_name
+  principal     = "cloudfront.amazonaws.com"
+  source_arn    = aws_cloudfront_distribution.cdn.arn
+  qualifier     = aws_lambda_function.basic_auth.version
 }
 
 resource "aws_iam_role" "lambda_exec" {
@@ -49,14 +79,14 @@ resource "aws_iam_role_policy" "lambda_logs" {
 # CloudFront Distribution
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
-    domain_name = var.bucket_regional_domain_name  # e.g., my-static-site-<account_id>.s3.us-east-1.amazonaws.com
+    domain_name = var.bucket_regional_domain_name
     origin_id   = "S3-${var.bucket_name}"
     s3_origin_config {
-      origin_access_identity = ""  # Public bucket
+      origin_access_identity = ""
     }
   }
   enabled             = true
-  default_root_object = "web/index.html"  # Serve /web/ by default
+  default_root_object = "web/index.html"
   default_cache_behavior {
     viewer_protocol_policy = "redirect-to-https"
     allowed_methods        = ["GET", "HEAD"]
