ca5844c5ab04572a1297d5f089bd6e0741e60a93
saulthompson <saulthompson@hotmail.co.uk>
2025-03-23 17:58:37 -0500

    add inputs for workflow_dispatch, and reinitialize terraform with the s3 backend in the bootstrap job after first setting up the s3 bucket

--- modules/bootstrap/main.tf
+++ modules/bootstrap/main.tf
@@ -1,17 +1,17 @@
 provider "aws" {
-  region = "${{ env.AWS_REGION }}"
+  region = var.aws_region
 }
 
 data "aws_caller_identity" "current" {}
 
 module "tfstate_s3" {
   source     = "../tfstate_s3"
-  account_id = data.aws_caller_identity.current.account_id
+  account_id = var.account_id
 }
 
 module "github_actions_integration" {
   source      = "../github_actions_integration"
   account_id  = data.aws_caller_identity.current.account_id
-  github_repo = "${{ github.repository }}"
+  github_repo = var.github_repo
   bucket_arn  = module.tfstate_s3.bucket_arn
 }
