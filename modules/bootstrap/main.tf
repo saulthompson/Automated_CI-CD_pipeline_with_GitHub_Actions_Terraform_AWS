9d753ea5ecbe1279bb67517d57b2f3cd45c6a3a8
saulthompson <saulthompson@hotmail.co.uk>
2025-03-23 19:01:53 -0500

    remove backend block from bootstrap/main.tf

--- modules/bootstrap/main.tf
+++ modules/bootstrap/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {} # partial backend config completed using cli args on terraform apply
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.aws_region
 }
